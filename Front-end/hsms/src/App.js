b105fca0c68ac33d23c7841d670a479d3f59a13f
Shubham <shubhamrahinj099@gmail.com>
2024-02-16 18:35:03 +0530

    Updated the App.js after necessary changes

--- Front-end/hsms/src/App.js
+++ Front-end/hsms/src/App.js
@@ -1,45 +1,5 @@
 
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import Header from './layouts/Header.jsx';
-// import Footer from './layouts/Footer.jsx';
-// import About from './pages/About.jsx';
-// import Signup from './pages/Signup.jsx';
-// import Home from './pages/Home.jsx';
-// import Login from './pages/Login.jsx';
-// import AddBuilding from './component/addbuilding.jsx';
-// import AddFlat from './component/addflat.jsx';
-// import AddUser from './component/adduser.jsx';
-// import ViewBuilding from './pages/ViewBuilding.jsx';
-// import PrivacyPolicy from './pages/PrivacyPolicy';
-// import Contact from './pages/Contact'; // Import Contact component
 
-// function App() {
-//   return (
-//     <>
-//       <div className="App container-fluid p-0 m-0">
-//         <Header />
-//         <div className='section'>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/signup" element={<Signup />} />
-//             <Route path="/add-building" element={<AddBuilding />} />
-//             <Route path="/add-flat" element={<AddFlat />} />
-//             <Route path="/add-user" element={<AddUser />} />
-//             <Route path="/view-building" element={<ViewBuilding />} />
-//             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-//             <Route path="/contact" element={<Contact />} /> {/* New route for Contact page */}
-//           </Routes>
-//         </div>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
@@ -54,6 +14,7 @@ import AddFlat from './component/addflat.jsx';
 import AddUser from './component/adduser.jsx';
 import ViewBuilding from './pages/ViewBuilding.jsx';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import Contact from './pages/Contact'; // Import Contact component
 import Terms from './pages/Terms'; // Import Terms component
 
 function App() {
@@ -73,6 +34,7 @@ function App() {
             <Route path="/view-building" element={<ViewBuilding />} />
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<Terms />} /> {/* New route for Terms page */}
+            <Route path="/contact" element={<Contact />} /> {/* New route for Contact page */}
           </Routes>
         </div>
         <Footer />
